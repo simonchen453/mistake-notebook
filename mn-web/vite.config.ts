815bf89a5fcb6e3fdf1f3b9c838225f332eed297
hongwei <hongwei@chenhongweideMacBook-Pro.local>
2025-12-05 23:34:11 +0800

    init

--- mn-web/vite.config.ts
+++ mn-web/vite.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:8080/mn',
-                changeOrigin: true
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/mn/api')
             }
         }
     }
